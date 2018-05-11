aa423c927d310cad5a95b0c8b13168f611abec4a
Filipe Ximenes <filipeximenes@gmail.com>
2018-05-11 00:10:39 -0300

    removes self references from first class functions strategy approach

--- strategy3.py
+++ strategy3.py
@@ -1,20 +1,20 @@
 from strategy import Package
 
 
-def bike_deliver(self):
-    print('The', self.package.content, 'is going to be delivered by Bike')
+def bike_deliver(package):
+    print('The', package.content, 'is going to be delivered by Bike')
     print('The cost will be $10')
     print('The ETA is 20 days')
 
 
-def truck_deliver(self):
-    print('The', self.package.content, 'is going to be delivered by Truck')
+def truck_deliver(package):
+    print('The', package.content, 'is going to be delivered by Truck')
     print('The cost will be $30')
     print('The ETA is 13d ays')
 
 
-def plane_deliver(self):
-    print('The', self.package.content, 'is going to be delivered by Plane')
+def plane_deliver(package):
+    print('The', package.content, 'is going to be delivered by Plane')
     print('The cost will be $60')
     print('The ETA is 2 days')
 
